f49890e1fcb5e3979ae8b6a0f29ac5af396ee53a
tsuji-108 <atsuhiro-tsuji@gaji.jp>
2025-12-07 23:39:02 +0900

    ユーザー投稿機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "/boards", to: "boards#index"
   get "/boards/:board_id", to: "boards#show"
 
-  get "/boards/:board_id/thread/:chatThread_id", to: "chat_threads#show"
+  get "/boards/:board_id/thread/:chatThread_id", to: "chat_threads#show", as: :board_chat_thread
+  post "/boards/:board_id/thread/:chatThread_id/posts", to: "posts#create", as: :chat_thread_posts
 end
